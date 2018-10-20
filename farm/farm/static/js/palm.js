9ae714bb1ec37758bb07c6732caab0d5c1981def
amazing-man <37244101+hagull@users.noreply.github.com>
2018-10-20 17:07:13 +0900

    .

--- farm/farm/static/js/palm.js
+++ farm/farm/static/js/palm.js
@@ -1,1 +1,21 @@
+function abc() {
+  if( window.screen.width > 768){
+    m_btn(value='on');
+  }
+  else {
+    m_btn(value='off');
+  }
+}
 
+
+function m_btn(){
+  var c_nav = document.getElementsByClassName('control-nav');
+  var value = "off";
+  if( this.value == 'off') {
+    this.value = 'on';
+    c_nav[0].style.display = 'none';
+  } else {
+    this.value='off';
+    c_nav[0].style.display = 'block';
+  }
+}
